e8cafae9885b5656142eab18792e3ef5f82ab751
GonsSssss <gmlrhs2@gmail.com>
2024-07-16 18:20:07 +0900

    heegon

---
+++ sql/user_table.sql
@@ -0,0 +1,16 @@
+create schema acount_book;
+
+create table user (
+user_id varchar(20), 
+password varchar(20), 
+user_name varchar(20), 
+sns_id varchar(20), 
+login_type varchar(20), 
+date_time datetime,
+user_image varchar(200)
+
+primary key (`user_id`),
+
+;
+
+
