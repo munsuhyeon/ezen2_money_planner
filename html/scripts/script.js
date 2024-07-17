ee74c0c6dad845a5415d7b45b186f4703cc2eba4
munsuhyeon <hrno3826@naver.com>
2024-07-17 18:18:04 +0900

    지출내역 작성중

---
+++ html/scripts/script.js
@@ -0,0 +1,13 @@
+function showTab(tabName) {
+    var tabs = document.getElementsByClassName('tab-content');
+    for (var i = 0; i < tabs.length; i++) {
+        tabs[i].style.display = 'none';
+    }
+    document.getElementById(tabName).style.display = 'table-row-group';
+
+    var tabButtons = document.getElementsByClassName('tab');
+    for (var i = 0; i < tabButtons.length; i++) {
+        tabButtons[i].classList.remove('active');
+    }
+    event.currentTarget.classList.add('active');
+}
