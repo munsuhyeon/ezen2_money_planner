72e45284503c3605dabdb60b85c8ddd3a5648b46
kiyohwa <elfcongru@naver.com>
2024-07-24 17:55:30 +0900

    통계

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,14 @@ import TransactionList from "./View/transcation/TransactionList";
 import Main from "./View/Main/Main.js";
 import MonthStatistics from "./Ui/MonthStatistics.js";
 import WeakStatistics from "./Ui/WeakStatistics.js";
+import SideNav from "./Components/SideNav/SideNav.js";
+import Header from "./Components/Header/Header.js";
 function App() {
   return (
     <div>
       <BrowserRouter>
+        <Header />
+        <SideNav />
         <Routes>
           {/*<Route path="/" element={}/>*/}
           <Route path="/" element={<Main />} />
