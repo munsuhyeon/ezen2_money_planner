8ada3cae1f07aaa9584676cbc4e033783eae192b
GonsSssss <gmlrhs2@gmail.com>
2024-08-02 13:05:01 +0900

    백업

--- client/src/Components/Login/Signup.jsx
+++ client/src/Components/Login/Signup.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import axios from 'axios'
-import {call} from "../service/ApiService"
+import { call } from "../service/ApiService"
 import "./Signup.css";
 
 
@@ -14,36 +14,61 @@ function Signup() {
 
     const Server_URL = "http://localhost:8080";
 
-    async function reqSignUp () {
-        const formData = {
-            userid : id,
-            password : password,
-            checkPassword : checkPassword,
-            username : name,
-            email : email 
+    const checkId = async () => {
+
+        try {
+            const response = await fetch(`http://localhost:8080/user/exists`, { 
+                method: 'POST',
+                headers: {'Content-Type': 'application/json',},
+                                body: JSON.stringify({ userid: id })});
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const exists = await response.json();
+            console.log(exists)
+            if (exists == 0) {
+                alert("사용 가능한 ID 입니다.")
+            }
+            else {
+                alert("이미 사용된 ID 입니다.")
+            }
+
+        } catch (error) {
+            console.error('Error checking user existence:', error);
         }
-        console.log("Call!!!!")
+    };
+
+
+    async function reqSignUp() {
+        const formData = {
+            userid: id,
+            password: password,
+            checkPassword: checkPassword,
+            username: name,
+            email: email
+        }
         call('/user/signup', 'POST', formData)
             .then((response) => {
                 console.log(response);
             })
-            .catch(error => console.error("저장 실패", error));
-            /*
-        const response = await axios.post(`${Server_URL}/user/signup`, {
-            userid : id,
-            password : password,
-            checkPassword : checkPassword,
-            username : name,
-            email : email 
-        })*/
+            .catch(error => console.error("회원가입 실패", error), alert("회원가입 실패"));
+        //    const response = await axios.post(`${Server_URL}/user/signup`, {
+        //         userid : id,
+        //         password : password,
+        //         checkPassword : checkPassword,
+        //         username : name,
+        //         email : email 
+        //     })
     }
+
 
     return (
         <div className="signup_section">
             <h2 className="signup_title_text">회원가입</h2>
             <div className="signup_input_container">
                 <div className="input_box">
-                    <h4 className="input_title">ID</h4>
+                    <h4 className="input_title">ID
+                        <button onClick={checkId}>중복 확인</button></h4>
                     <input type="text" value={id} onChange={(e) => setId(e.target.value)} placeholder="ID를 입력해주세요" />
                 </div>
                 <div className="input_box">
