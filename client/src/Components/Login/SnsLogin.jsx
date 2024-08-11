e6e038d6f7ba9835670df089fc1748ff3b316a4d
GonsSssss <gmlrhs2@gmail.com>
2024-08-12 01:28:16 +0900

    카카오로그인백업

--- client/src/Components/Login/SnsLogin.jsx
+++ client/src/Components/Login/SnsLogin.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
+import axios from "axios";
 
 const kakaoLogin = (e) => {
     e.preventDefault();
@@ -16,7 +17,7 @@ useEffect(() => {
     const searchParams = new URLSearchParams(location.search);
     const code = searchParams.get('code');
     axios.post(`https://kauth.kakao.com/oauth/token`, null, {
-        headers: {},
+        headers: { "Content-Type": application / x - www - form - urlencoded },
         params: {
             grant_type: "authorizatio_code",
             client_id: process.env.REACT_APP_Kakao_clientId,
@@ -26,5 +27,38 @@ useEffect(() => {
     })
     .then((res) => {
         console.log(res)
+        axios.post(`${process.env.REACT_APP_HOST}/api/getKakaoUser`, null, {
+          headers:{"data" : JSON.stringify(res.data)}
+        })
+        .then((res) => {
+            console.log(res)
+          
+            if (res.data.message == "no user") {
+                console.log(res.data)
+                alert("회원가입 정보가 없어 회원가입화면으로 넘어갑니다")
+                navigate('/register', { state: { userEmail: res.data.userEmail, userType: 'K' } })
+            } else if (res.data.message == "active user") {
+     
+                const accessToken = res.data.accessToken;
+                axios.defaults.headers.common['authorization'] = `Bearer ${accessToken}`;
+                localStorage.setItem("loggedIn", true); 
+                localStorage.setItem("userid", res.data.userInfo.user_id);
+                localStorage.setItem("username", res.data.userInfo.user_name);
+                localStorage.setItem("accessToken", accessToken);
+                localStorage.setItem("tokenExp", res.data.tokenExp);
+                navigate('/');
+                window.location.reload();
+            }
+        })
+        .catch((err) => {
+            console.log("토큰전달 에러  ::   ", err)
+            alert("로그인 시도 중 오류가 발생하였습니다")
+            navigate('/login');
+        })
+})
+    .catch((err) => {
+        console.log(err)
+        alert("로그인 시도 중 오류가 발생하였습니다")
+        navigate('/login');
     })
 }, [location]);
